cb39eb81b89f14152a4e9b58b3f9f6b1df21305b
Mathieu Schaff <schaff.mathieu93@gmail.com>
2022-12-02 12:10:30 +0100

    exam 3 finish

---
+++ exam3/src/exam3.js
@@ -0,0 +1,12 @@
+import { render } from "./utils/render.js";
+
+render(
+  {
+    type: "img",
+    props: {
+      src: "https://media.auchan.fr/d87f5bf0-733c-417f-a3f6-7e57d6dd898c_256x256/B2CD/",
+    },
+    children: [""],
+  },
+  document.body
+);
